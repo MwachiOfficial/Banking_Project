e5f8628e9210bed8a790949129916c69d74431d0
EMT-MOSES <elvis.tangasaiti@strathmore.edu>
2025-06-18 10:08:33 +0300

    CORRECTED CODE

--- main.cpp
+++ main.cpp
@@ -1,45 +1,64 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
-int normal_user(){
 
+int normal_user() {
+    return 0;
 }
-int admin(){
-    string username;
-    string admin_password;
-    cout<<"Welcome to the registration and deposit page\n";
-    cout<<"Enter administrator username and person.\n";
-    ifstream fin;
-    ifstream fout;
-    fin.open("admin.txt");
-    cout<<"Enter username:\t";
-    fin<<admin_username"\n";
-    fout<<"Enter password: \t"
-    fin<<admin_password"\n";
-    fin.close();
-    welcome_admin();
 
-}
-int welcome_admin(){
+int welcome_admin() {
     int option;
-    cout<<"1. Deposit money for client \n";
-    cout<<"2. Deposit money for client \n";
-    cout<<"3. Deposit money for client \n";
-    cin<<option;
-    
-
+    cout << "1. Deposit money for client\n";
+    cout << "2. Withdraw money for client\n";
+    cout << "3. Check client balance\n";
+    cout << "Enter your choice: ";
+    cin >> option;
+    return 0;
 }
-int main(){
-    int option
-    cout<<"Welcome to xxx E_CASH SERVICES ********\n";
-    cout<<"Choose option;\n"
-    cout<<"Option 1: Normal user login\n";
-    cout<<"Option 2: Administrator login\n";
-    cin<< option;
-    if (option=1){
-        admin();
+int admin() {
+    string admin_username;
+    string admin_password;
+
+    cout << "Welcome to the registration and deposit page\n";
+    cout << "Enter administrator username and password.\n";
+
+    cout << "Enter username:\t";
+    cin >> admin_username;
+
+    cout << "Enter password:\t";
+    cin >> admin_password;
+
+    ofstream fout("admin.txt");
+    if (!fout) {
+        cout << "Error opening file for writing.\n";
+        return 1;
     }
-    else if(
+
+    fout << admin_username << "\n" << admin_password << "\n";
+    fout.close();
+
+    welcome_admin();
+    return 0;
+}
+
+int main() {
+    int option;
+
+    cout << "Welcome to xxx E_CASH SERVICES ********\n";
+    cout << "Choose option:\n";
+    cout << "Option 1: Normal user login\n";
+    cout << "Option 2: Administrator login\n";
+    cout << "Enter your option: ";
+    cin >> option;
+
+    if (option == 1) {
         normal_user();
-    )
+    } else if (option == 2) {
+        admin();
+    } else {
+        cout << "Invalid option selected.\n";
+    }
+
+    return 0;
 }
